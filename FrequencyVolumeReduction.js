3baf2de05b0e9a4ddb3cb55123490080519b3bae
Amateur Audio Dude <168192910+AmateurAudioDude@users.noreply.github.com>
2024-10-21 22:14:07 +1100

    v1.0.4

--- FrequencyVolumeReduction.js
+++ FrequencyVolumeReduction.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Frequency Volume Reduction',
-    version: '1.0.3',
+    version: '1.0.4',
     author: 'AAD',
     frontEndPath: 'FrequencyVolumeReduction/pluginFrequencyVolumeReduction.js'
 }
